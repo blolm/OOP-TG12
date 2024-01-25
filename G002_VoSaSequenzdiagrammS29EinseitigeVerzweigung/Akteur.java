4e5c08370e298c408d5ace3d1829652247f2f40b
blolm <125360625+blolm@users.noreply.github.com>
2024-01-25 11:54:43 +0000

    Unterricht.25.01.2024

--- G002_VoSaSequenzdiagrammS29EinseitigeVerzweigung/Akteur.java
+++ G002_VoSaSequenzdiagrammS29EinseitigeVerzweigung/Akteur.java
@@ -1,10 +1,29 @@
 package G002_VoSaSequenzdiagrammS29;
 
+import java.util.ArrayList;
+
 public class Akteur {
 
     public static void main(String[] args) {
         Klasse1a object1 = new Klasse1a();
 
         object1.op1();
+
+        ArrayList<String> tg12 = new ArrayList();
+
+        tg12.add("max");
+        tg12.add("moritz");
+        tg12.add("fritz");
+        
+        for (String s : tg12) {
+        System.out.println(s);
+        
+        }
+
+        System.out.println("-------");
+
+        for (int i; i < tg12.size(); i++){
+            System.out.println(tg12.get(i));
+        }
     }
 }
